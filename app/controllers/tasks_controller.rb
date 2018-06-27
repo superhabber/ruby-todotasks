13371f35d76269d4f10bb2a9b09c957dc7268372
Yaroslav Palamarchuk <superhabber@gmail.com>
2018-06-27 14:47:59 +0300

    Update tasks_controller.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,13 +17,12 @@ class TasksController < ApplicationController
 
   def edit
     @task = Task.find params[:id]
-    redirect_to tasks_path
   end
 
   def update
     @task = Task.find params[:id]
 
-    @task.updated_attributes task_params
+    @task.update_attributes task_params
 
     redirect_to tasks_path
   end
